c243ee46ae5360715428e729a47cc9abc3e02b07
Rahul-D78 <rahulparida933@gmail.com>
2021-07-20 01:39:10 +0530

    feat: basic homepage skeletone done
    Signed-off-by: Rahul-D78 <rahulparida933@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,20 +13,11 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <h1>
-          <Navbar/>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
         <div className={styles.grid}>
+          <h1 className={styles.title}>
+            Welcome to <a href="https://nextjs.org">Hue!</a>
+          </h1>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
