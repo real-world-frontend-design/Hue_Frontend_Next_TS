b2f204fc87cd8a49f27cacb8906d788f25c33b95
Rahul-D78 <rahulparida933@gmail.com>
2021-07-19 23:57:09 +0530

    chore: add some constants
    Signed-off-by: Rahul-D78 <rahulparida933@gmail.com>

---
+++ lib/utils/constants.ts
@@ -0,0 +1,8 @@
+export const SERVER_BASE_URL = `https://demo.realworld.io/`;
+
+export const APP_NAME = `Hue`;
+
+export const ARTICLE_QUERY_MAP = {
+  "tab=feed": `${SERVER_BASE_URL}/articles/feed`,
+  "tab=tag": `${SERVER_BASE_URL}/articles/tag`
+};
