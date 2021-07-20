ca3cd9a44a0a5c42f45aae36623627082f80905a
Rahul-D78 <rahulparida933@gmail.com>
2021-07-20 17:04:13 +0530

    feat: implement article layout
    Signed-off-by: Rahul-D78 <rahulparida933@gmail.com>

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React from "react";
+import {
+    Typography, 
+    Card, 
+    CardActionArea, 
+    Avatar, 
+    CardMedia, 
+    CardContent, 
+    CardActions,
+    Button } from "@material-ui/core";
+import moment from "moment"
+import { AiFillDelete, AiFillHeart } from 'react-icons/ai';
+import styles from '../styles/Feed.module.css';
 
 export default function Feed() {
-    return (
-        <h1>
-            This is where all the feed items will be rendered.
-        </h1>
-    );
+
+  return (
+    <div>
+      <div className={styles.center}>
+        <Card className={styles.root}>
+          <CardActionArea>
+            <CardMedia
+              className={styles.media}
+              image="https://res.cloudinary.com/practicaldev/image/fetch/s--yu7IJWqw--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/y25pmk3wa67d3m7m5eu3.png"
+              title="devops lover"
+            />
+            <CardContent style={{paddingBottom: "0"}}>
+              <div className={styles.row}>
+                <Avatar sizes="small"/>
+                <Typography
+                  style={{ marginLeft: "5px", marginTop: "0px" }}
+                  gutterBottom
+                >
+                DevOps
+                </Typography>
+              </div>
+              
+              <Typography variant="body2"> {moment('2021-07-19 00:23:43.165809').fromNow()}  </Typography>
+                  
+              <Typography variant="h5" color="primary" component="h5" >
+               DevOps
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary"><AiFillHeart fontSize="small" /> Like 1 </Button>
+            <div className={styles.right}>
+                <AiFillDelete/>
+            </div>
+          </CardActions>
+        </Card>
+      </div>
+    </div>
+  );
 }
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,14 @@
 import Link from "next/link";
 import styles from "../styles/Navbar.module.css"
 import Navbar  from '../components/commons/NavBar';
+import Feed from "../components/Feed";
 
 export default function about() {
     return (
         <div>
             <Navbar/>
             <div className={styles.main}>
-                <Link href="/">
-                    <a>Home</a>
-                </Link>
-                <h1>About</h1>
+                <Feed/>     
             </div>
         </div>
     );
